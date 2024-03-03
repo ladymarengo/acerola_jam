20ee36efe2b74d9e5d3d48b311017cdaeebcc4c3
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2024-03-03 11:28:59 +0200

    Add merge

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,15 @@ struct Phase(u8);
 #[derive(Component)]
 struct MainCamera;
 
-#[derive(Component)]
-struct Selected;
-
 #[derive(Resource)]
-struct SelectedEntities(Vec<Entity>);
+struct SelectedEntity(Option<SelectionOptions>);
 
-#[derive(Component)]
-struct SelectedId(Option<Entity>);
+// #[derive(Resource)]
+struct SelectionOptions {
+    entity: Entity,
+    sprite: Entity,
+    phase: u8,
+}
 
 fn main() {
     println!("Hello, jam!");
@@ -30,7 +31,7 @@ fn main() {
             ..default()
         }))
         .insert_resource(ClearColor(Color::ALICE_BLUE))
-        .insert_resource(SelectedEntities(Vec::new()))
+        .insert_resource(SelectedEntity(None))
         .add_systems(Startup, (spawn_camera, spawn_shapes))
         .add_systems(Update, (bevy::window::close_on_esc, mouse_motion))
         .run();
@@ -62,7 +63,6 @@ fn spawn_shapes(
                     ..default()
                 },
                 Phase(0),
-                SelectedId(None),
             ));
         }
     }
@@ -71,17 +71,11 @@ fn spawn_shapes(
 fn mouse_motion(
     q_window: Query<&Window, With<PrimaryWindow>>,
     q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
-    mut query: Query<(
-        &mut Phase,
-        &mut TextureAtlas,
-        &Transform,
-        &mut SelectedId,
-        Entity,
-    )>,
+    mut query: Query<(&mut Phase, &mut TextureAtlas, &Transform, Entity)>,
     mouse_button_input: Res<ButtonInput<MouseButton>>,
-    mut selected: ResMut<SelectedEntities>,
+    mut selected: ResMut<SelectedEntity>,
     mut commands: Commands,
-    asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
 ) {
     let (camera, camera_transform) = q_camera.single();
     let window = q_window.single();
@@ -90,53 +84,44 @@ fn mouse_motion(
         .cursor_position()
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
         .map(|ray| ray.origin.truncate())
-		{
-
-		if mouse_button_input.just_pressed(MouseButton::Right) && selected.0.len() == 2 {
-			for (mut phase, mut sprite, _transform, _selected_id, entity) in &mut query {
-				if selected.0[1] == entity {
-					phase.0 += 1;
-					sprite.index = if phase.0 < 5 { phase.0.into() } else { 4 };
-				}
-			}
-			commands.entity(selected.0[0]).despawn();
-			selected.0.clear();
-			return;
-		}
+    {
 
         if mouse_button_input.just_pressed(MouseButton::Left) {
-            for (mut phase, mut sprite, transform, mut selected_id, entity) in &mut query {
+            for (mut phase, mut sprite, transform, entity) in &mut query {
                 if (transform.translation.x - world_position.x).abs() < 50.0
                     && (transform.translation.y - world_position.y).abs() < 50.0
                 {
-                    if let Some(id) = selected_id.0 {
-                        commands.entity(id).despawn();
-                        selected_id.0 = None;
-                        let index = selected.0.iter().position(|x| *x == entity).unwrap();
-                        selected.0.remove(index);
-                    } else if selected.0.len() < 2 {
-                        selected.0.push(entity);
-
-                        selected_id.0 = Some(
-                            commands
-                                .spawn((
-                                    SpriteBundle {
-                                        texture: asset_server.load("test_selection.png"),
-                                        transform: Transform::from_xyz(
-                                            transform.translation.x,
-                                            transform.translation.y,
-                                            0.0,
-                                        ),
-                                        ..default()
-                                    },
-                                    Selected,
-                                ))
-                                .id(),
-                        );
+                    if let Some(selection) = &selected.0 {
+                        if selection.entity == entity {
+                            commands.entity(selection.sprite).despawn();
+                            selected.0 = None;
+                        } else if selection.entity != entity && selection.phase == phase.0 {
+                            commands.entity(selection.entity).despawn();
+                            commands.entity(selection.sprite).despawn();
+                            selected.0 = None;
+                            phase.0 += 1;
+                            sprite.index = if phase.0 < 5 { phase.0.into() } else { 4 };
+                        }
+                    } else {
+                        let sprite = commands
+                            .spawn(SpriteBundle {
+                                texture: asset_server.load("test_selection.png"),
+                                transform: Transform::from_xyz(
+                                    transform.translation.x,
+                                    transform.translation.y,
+                                    0.0,
+                                ),
+                                ..default()
+                            })
+                            .id();
+                        selected.0 = Some(SelectionOptions {
+                            entity,
+                            sprite,
+                            phase: phase.0,
+                        });
                     }
                 }
             }
         }
-
     }
 }
