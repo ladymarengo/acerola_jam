9ca463398d8b7f6ab474050065c00f4767970f27
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2024-03-10 11:08:51 +0200

    Split phase colors and face expressions into separate sprites

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::too_many_arguments, clippy::type_complexity)]
+
 use bevy::{prelude::*, window::PrimaryWindow};
 use rand::Rng;
 
@@ -18,7 +20,7 @@ fn main() {
         .insert_resource(ClearColor(Color::ALICE_BLUE))
         .insert_resource(SelectedEntity(None))
         .insert_resource(CursorCoords(None))
-        .add_systems(Startup, (spawn_camera, spawn_shapes))
+        .add_systems(Startup, (spawn_camera, spawn_smilers))
         .add_systems(
             Update,
             (
@@ -82,60 +84,100 @@ fn spawn_new_cells(
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
     query: Query<&Transform, With<Cell>>,
 ) {
-    let texture = asset_server.load("test_shapes.png");
-    let layout = TextureAtlasLayout::from_grid(Vec2::new(100.0, 100.0), 4, 3, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    let texture_expr = asset_server.load("expressions.png");
+    let layout_expr = TextureAtlasLayout::from_grid(Vec2::new(200.0, 200.0), 2, 1, None, None);
+    let texture_atlas_layout_expr = texture_atlas_layouts.add(layout_expr);
+
+    let texture_color = asset_server.load("colors.png");
+    let layout_color = TextureAtlasLayout::from_grid(Vec2::new(200.0, 200.0), 3, 2, None, None);
+    let texture_atlas_layout_color = texture_atlas_layouts.add(layout_color);
 
     let mut rng = rand::thread_rng();
 
-    for x in (-240..=240).step_by(120) {
+    for x in (-240..=120).step_by(120) {
         if !query.iter().any(|transform| {
-            transform.translation.x == x as f32 && transform.translation.y >= 240.0
+            transform.translation.x == x as f32 && transform.translation.y >= 120.0
         }) {
-            let corrupted = rng.gen::<f64>() < 0.4;
-            commands.spawn((
-                SpriteSheetBundle {
-                    texture: texture.clone(),
-                    atlas: TextureAtlas {
-                        layout: texture_atlas_layout.clone(),
-                        index: if corrupted { 5 } else { 0 },
+            let corrupted = rng.gen::<f64>() < 0.7;
+            commands
+                .spawn((
+                    SpriteSheetBundle {
+                        texture: texture_expr.clone(),
+                        atlas: TextureAtlas {
+                            layout: texture_atlas_layout_expr.clone(),
+                            index: if corrupted { 1 } else { 0 },
+                        },
+                        transform: Transform::from_xyz(x as f32, 360.0, 1.0)
+                            .with_scale(Vec3::splat(0.5)),
+                        ..default()
                     },
-                    transform: Transform::from_xyz(x as f32, 360.0, 0.0),
-                    ..default()
-                },
-                Phase(0),
-                Cell,
-                Corrupted(corrupted),
-            ));
+                    Phase(0),
+                    Cell,
+                    Corrupted(corrupted),
+                ))
+                .with_children(|parent| {
+                    parent.spawn((
+                        SpriteSheetBundle {
+                            texture: texture_color.clone(),
+                            atlas: TextureAtlas {
+                                layout: texture_atlas_layout_color.clone(),
+                                index: 0,
+                            },
+                            transform: Transform::from_xyz(0.0, 0.0, -5.0),
+                            ..default()
+                        },
+                        SmilerColor,
+                    ));
+                });
         }
     }
 }
 
-fn spawn_shapes(
+fn spawn_smilers(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    let texture = asset_server.load("test_shapes.png");
-    let layout = TextureAtlasLayout::from_grid(Vec2::new(100.0, 100.0), 4, 3, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    let texture_expr = asset_server.load("expressions.png");
+    let layout_expr = TextureAtlasLayout::from_grid(Vec2::new(200.0, 200.0), 2, 1, None, None);
+    let texture_atlas_layout_expr = texture_atlas_layouts.add(layout_expr);
 
-    for x in (-240..=240).step_by(120) {
-        for y in (-240..=240).step_by(120) {
-            commands.spawn((
-                SpriteSheetBundle {
-                    texture: texture.clone(),
-                    atlas: TextureAtlas {
-                        layout: texture_atlas_layout.clone(),
-                        index: 0,
+    let texture_color = asset_server.load("colors.png");
+    let layout_color = TextureAtlasLayout::from_grid(Vec2::new(200.0, 200.0), 3, 2, None, None);
+    let texture_atlas_layout_color = texture_atlas_layouts.add(layout_color);
+
+    for x in (-240..=120).step_by(120) {
+        for y in (-240..=120).step_by(120) {
+            commands
+                .spawn((
+                    SpriteSheetBundle {
+                        texture: texture_expr.clone(),
+                        atlas: TextureAtlas {
+                            layout: texture_atlas_layout_expr.clone(),
+                            index: 0,
+                        },
+                        transform: Transform::from_xyz(x as f32, y as f32, 1.0)
+                            .with_scale(Vec3::splat(0.5)),
+                        ..default()
                     },
-                    transform: Transform::from_xyz(x as f32, y as f32, 0.0),
-                    ..default()
-                },
-                Phase(0),
-                Cell,
-                Corrupted(false),
-            ));
+                    Phase(0),
+                    Cell,
+                    Corrupted(false),
+                ))
+                .with_children(|parent| {
+                    parent.spawn((
+                        SpriteSheetBundle {
+                            texture: texture_color.clone(),
+                            atlas: TextureAtlas {
+                                layout: texture_atlas_layout_color.clone(),
+                                index: 0,
+                            },
+                            transform: Transform::from_xyz(0.0, 0.0, -5.0),
+                            ..default()
+                        },
+                        SmilerColor,
+                    ));
+                });
         }
     }
 }
@@ -146,18 +188,23 @@ fn mouse_motion(
     cursor_coords: Res<CursorCoords>,
     mouse_button_input: Res<ButtonInput<MouseButton>>,
     mut selected: ResMut<SelectedEntity>,
-    mut query: Query<(
-        &mut Phase,
-        &mut TextureAtlas,
-        &Transform,
-        Entity,
-        &mut Corrupted,
-    )>,
+    mut query: Query<
+        (
+            &mut Phase,
+            &mut TextureAtlas,
+            &Transform,
+            Entity,
+            &mut Corrupted,
+            &Children,
+        ),
+        Without<SmilerColor>,
+    >,
     texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    mut colors: Query<&mut TextureAtlas, With<SmilerColor>>,
 ) {
     if let Some(cursor_coords) = cursor_coords.0 {
         if mouse_button_input.just_pressed(MouseButton::Left) {
-            for (mut phase, mut sprite, transform, entity, mut corrupted) in &mut query {
+            for (mut phase, mut sprite, transform, entity, mut corrupted, children) in &mut query {
                 if (transform.translation.x - cursor_coords.x).abs() < 50.0
                     && (transform.translation.y - cursor_coords.y).abs() < 50.0
                 {
@@ -175,15 +222,17 @@ fn mouse_motion(
                             if (corrupted.0 || selection.corrupted)
                                 && !(corrupted.0 && selection.corrupted)
                             {
-                                corrupted.0 = rng.gen::<f64>() < 0.6;
+                                corrupted.0 = rng.gen::<f64>() < 0.9;
                             }
-                            if phase.0 < 5 {
+                            if phase.0 < 6 {
+                                let child = children.first().unwrap();
+                                let mut color_sprite = colors.get_mut(*child).unwrap();
+                                color_sprite.index += 1;
+                            }
+                            if corrupted.0 && sprite.index < 1 {
                                 sprite.index += 1;
                             }
-                            if corrupted.0 && sprite.index < 5 {
-                                sprite.index += 5;
-                            }
-                            commands.entity(selection.entity).despawn();
+                            commands.entity(selection.entity).despawn_recursive();
                             commands.entity(selection.sprite).despawn();
                             selected.0 = None;
                         }
@@ -210,10 +259,10 @@ fn mouse_motion(
                 }
             }
         } else if mouse_button_input.just_pressed(MouseButton::Right) {
-            for (_phase, _sprite, _transform, entity, _corrupted) in &query {
-                commands.entity(entity).despawn();
+            for (_phase, _sprite, _transform, entity, _corrupted, _children) in &query {
+                commands.entity(entity).despawn_recursive();
             }
-            spawn_shapes(commands, asset_server, texture_atlas_layouts);
+            spawn_smilers(commands, asset_server, texture_atlas_layouts);
         }
     }
 }
@@ -226,6 +275,9 @@ struct MainCamera;
 
 #[derive(Component)]
 struct Cell;
+
+#[derive(Component)]
+struct SmilerColor;
 
 #[derive(Component)]
 struct Corrupted(bool);
